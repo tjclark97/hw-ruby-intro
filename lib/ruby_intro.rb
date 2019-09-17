d7af527b800a98b9eb94c1462c855f78f1b9c6a7
Ubuntu <ubuntu@ip-172-31-87-99.ec2.internal>
2019-09-17 00:37:16 +0000

    Pass all test

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,95 @@
 # Part 1
 
 def sum arr
+  count = 0
+  arr.each { |item|
+    count += item
+  }
+  return count
   # YOUR CODE HERE
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  max1 = arr.max
+  if max1 == nil
+    return 0
+  end
+  arr.delete_at(arr.index(max1))
+  max2 = arr.max
+  if max2 == nil
+    return max1
+  else
+    return max1 + max2
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.each { |i|
+    arr.each{ |j|
+      if i+j == n && i != j
+        return true
+      end
+    }
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  comp = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+  if (!(s.empty?)) && (comp.include? s[0])
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  
+  t = s.delete "10"
+  if s.empty? || !(t.empty?)
+    return false
+  end
+  
+  n = s.to_i(2)
+  if n % 4 == 0
+    return true
+  end
+  
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    if isbn.empty? || price <= 0
+      raise ArgumentError
+    end
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    #if @isbn < 1
+    #  return "$00" + to_s(@isbn)
+    n = price.to_s.split('.')
+    if n.size == 1
+      n[1] = "00"
+    elsif n[1].size > 2
+      n[1] = n[1][0] + n[1][1]
+    elsif n[1].size == 1
+      n[1] = n[1] + "0"
+    end
+    
+    return "$" + n[0] + "." + n[1]
+  end
+  
 end
